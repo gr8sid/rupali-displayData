a1394a1ed5f7de540b5f1bc334d45e794816c6c3
gr8sid <gr8sid7981@gmail.com>
2019-02-08 02:51:26 -0500

    connected to mlab database

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
     // local MongoDB deployment 
-    "URI":"mongodb://localhost/sidharth"
+    //"URI":"mongodb://localhost/sidharth"
+    "URI":"mongodb://sidharth:a123456@ds058739.mlab.com:58739/sidharth"
 }
